b8db827b33b5a66f01975690a26b914c137d9f93
RizkyAlfiansyah <rizkkyaf@gmail.com>
2022-02-08 01:26:20 +0800

    sidebar components

--- components/sidebar.js
+++ components/sidebar.js
@@ -1,5 +1,44 @@
-import react from "react";
+import SvgStore from '../public/icons/svgs/Store.js';
+import SvgHome from '../public/icons/svgs/Home.js';
+import SvgMessage from '../public/icons/svgs/Message.js';
+import SvgNotification from '../public/icons/svgs/Notification.js';
+import SvgLogout from '../public/icons/svgs/Logout.js';
+import SvgDiscount from '../public/icons/svgs/Discount.js';
+import SvgGraph from '../public/icons/svgs/Graph.js';
+import SvgSettings from '../public/icons/svgs/Settings.js';
 
-const Sidebar = (props) => {
-    
+const Sidebar = () => {
+    return (
+        <>
+            <div className="flex flex-col gap-y-4 items-center py-8 w-24 bg-gray-900">
+                <button className="p-2 bg-opacity-20 rounded-xl bg-primary">
+                    <SvgStore />
+                </button>
+                <div className='flex flex-col gap-y-4 items-end self-end'>
+                    <div className='bg-gray-800 rounded-l-xl relative before:absolute before:w-4 before:h-8 before:-top-8 before:rounded-br-xl before:right-0 before:shadow-inverse-top  after:absolute after:w-4 after:h-8 after:-bottom-8 after:rounded-tr-xl after:right-0 after:shadow-inverse-bottom'>
+                        <button className='p-4 my-4 mr-4 ml-3 rounded-xl hover:bg-primary '>
+                            <SvgHome />
+                        </button>
+                    </div>
+                    <button className='p-4 my-4 mr-4 ml-3 rounded-xl hover:bg-primary '>
+                            <SvgDiscount />
+                    </button>
+                    <button className='p-4 my-4 mr-4 ml-3 rounded-xl hover:bg-primary '>
+                            <SvgGraph />
+                    </button>
+                    <button className='p-4 my-4 mr-4 ml-3 rounded-xl hover:bg-primary '>
+                            <SvgNotification />
+                    </button>
+                    <button className='p-4 my-4 mr-4 ml-3 rounded-xl hover:bg-primary '>
+                            <SvgSettings />
+                    </button>
+                    <button className='p-4 my-4 mr-4 ml-3 rounded-xl hover:bg-primary '>
+                            <SvgLogout />
+                    </button>
+                </div>
+            </div>
+        </>
+    )
 }
+
+export default Sidebar;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,19 @@
+import React from 'react';
+import Sidebar from '../components/sidebar';
+
 export default function Home() {
   return (
+    <>
     <div className="flex w-full min-h-screen font-sans bg-gray-800">
-
+      <Sidebar />
+    <main className='flex flex-col flex-1 gap-6 p-4'>
+      <header>
+        <h1 className="text-3xl font-semibold leading-loose text-white">Dashboard</h1>
+        <div className="text-gray-200">Tuesday 2 Feb, 2021</div>
+      </header>
+      <hr class="border-gray-700" />
+    </main>
     </div>
+    </>
   )
 }
