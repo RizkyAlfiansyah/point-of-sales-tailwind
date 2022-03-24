6236ff489b8c44770494b1fd593e8bc26ca51b4b
RizkyAlfiansyah <rizkkyaf@gmail.com>
2022-03-24 23:02:38 +0800

    update

--- pages/index.js
+++ pages/index.js
@@ -15,11 +15,11 @@ export default function Home() {
         <h1 className="text-3xl font-semibold leading-loose text-white">Dashboard</h1>
         <div className="text-gray-200">Tuesday 2 Feb, 2021</div>
       </header>
-      <hr class="border-gray-700" />
+      <hr className="border-gray-700" />
       <StatsCard />
       <OrderReport />
     </main>
-    <aside class="flex flex-col gap-y-6 pt-6 pr-6 w-96">
+    <aside className="flex flex-col gap-y-6 pt-6 pr-6 w-96">
       <MostOrdered />
       <MostTypeOfOrder />
     </aside>    
